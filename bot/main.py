664c70a78ee0544d246447e8ee05a0627d19a6c9
Replit user <>
2023-09-16 21:39:30 +0000

    discord bot makes request to flask

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord.ext import commands
-
+import requests
 # initialize bot
 token = os.environ["BOT_TOKEN"]
 intents = discord.Intents.default()
@@ -12,4 +12,11 @@ bot = discord.Bot(intents=intents)
 async def test(ctx):
   await ctx.respond("https://storage.googleapis.com/dss-bucket/videos/Garfield%20Dancing%20to%20Happy.mp4")
 
+@bot.command()
+async def recordings(ctx):
+  url = "https://discord-security-system.subwayman.repl.co/video-index"
+  req = requests.get(url)
+  print(req.text)
+  print(eval(req.text))
+  await ctx.respond("check repl.")
 bot.run(token)
